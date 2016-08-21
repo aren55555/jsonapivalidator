a7ca2a4c4e1a0d2878962491c5d2ad8c1ed39fdc
Aren Patel <aren55555@users.noreply.github.com>
2016-08-21 16:52:40 -0700

    Remove unused func

--- meta.go
+++ meta.go
@@ -2,10 +2,6 @@ package jsonapivalidator
 
 import "fmt"
 
-// func validateMeta(meta interface{}, result *Result) {
-// 	fmt.Println("validateMeta")
-// }
-
 func validateMetaObject(m interface{}, result *Result) {
 	fmt.Println("validateMetaObject")
 }
